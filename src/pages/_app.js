6a40084afe7e1d83b8bdd8cdfbe3437ad5a2c7b4
João Maia <joaocarlosbgm@gmail.com>
2023-04-13 21:04:05 -0300

    initial commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import { ThemeProvider } from "styled-components";
+import { theme } from "../theme";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
---
+++ src/theme/index.jsx
@@ -0,0 +1,17 @@
+const theme = {
+  black1: "#0000",
+  white: "#FFFF",
+
+  dark1: "#1C1C1E",
+  dark2: "#2C2C2E",
+  dark3: "#3A3A3C",
+
+  gray1: "#9A9A9A",
+  gray2: "#CACACA",
+  gray3: "#F1F1F1",
+
+  orange1: "#FF9601",
+  orange2: "#FFC470",
+};
+
+export { theme };
